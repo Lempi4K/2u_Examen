d35a98cf19f477447f5bd163369ac429dd0b43b1
Lempi4K <lemopi2008@gmail.com>
2022-04-12 17:27:11 -0600

    0.3: Bases del efecto matrix

--- js/Sesion.js
+++ js/Sesion.js
@@ -1,11 +1,26 @@
+function readCookie(name) {
+    var nameEQ = name + "="; 
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) {
+        return decodeURIComponent( c.substring(nameEQ.length,c.length) );
+      }
+    }
+    return null;
+  }
+
 function botonMandar(){
     var usuario = document.getElementById("txtUsuario").value;
     var pass = document.getElementById("txtPass").value;
     if(usuario=="Lempi4K" && pass=="morenopinto23"){
-        alert("ta bien");
+        document.cookie="inicio="+encodeURIComponent("10")+"; expires="+new Date(2068, 1, 02, 11, 20).toUTCString+"; path=/"
+        location.reload();
     }
     else{
-        alert("ta mal");
+        alert("Usuario o contraseña incorrectos");
+        document.getElementById("txtPass").value="";
     }
 }
 
---
+++ js/Verificador.js
@@ -0,0 +1,64 @@
+$.getScript("otro-archivo.js", function(){
+    alert("Archivo cargado!");
+ });
+function readCookie(name) {
+    var nameEQ = name + "="; 
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) {
+        return decodeURIComponent( c.substring(nameEQ.length,c.length) );
+      }
+    }
+    return null;
+}
+function efectoMatrix(){
+    var separadores = document.getElementsByClassName("E_20px")
+    for(var i=0; i<separadores.length; i++){
+        separadores[i].style.display="none"
+    }
+    var body = document.querySelector("body");
+    body.style.position="relative";
+    var contenedorAll = document.getElementById("ContenedorAll");
+    contenedorAll.style.position="absolute";
+    contenedorAll.style.top="0";  
+    contenedorAll.style.left="50%"; 
+    contenedorAll.style.transform="translate(-50%, 30px)"; 
+    contenedorAll.style.zIndex="0";
+    var canvas = document.getElementById("canvas");
+    canvas.style.display="block";
+    canvas.height = $(document).height();
+    canvas.width = document.body.clientWidth;
+    var columns = []
+    for (i = 0; i < 256; columns[i++] = 1);
+        function step() {
+            canvas.getContext('2d').fillStyle = 'rgba(0,0,0,0.05)';
+            canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height);
+            canvas.getContext('2d').fillStyle = '#0F0';
+            columns.map(function (value, index) {
+                var character = String.fromCharCode(3e4 + Math.random() * 33);
+                canvas.getContext('2d').fillText(character, index * 10, value);
+                columns[index] = value > 758 + Math.random() * 1e4 ? 0 : value + 10
+        })
+    }
+    setInterval(step, 33)
+}
+function verificador(){
+    var pestaña = document.getElementById("conf");
+    var inicioSesion = document.getElementById("IS");
+    //alert(readCookie("inicio")=="10")
+    if(readCookie("inicio")=="10"){
+        pestaña.style.display="block";
+        inicioSesion.style.display="none";
+    }else{
+        pestaña.style.display="none";
+        inicioSesion.style.display="block";
+    }
+
+    if(readCookie("fxMatrix")=="10"){
+        efectoMatrix();
+    }else{
+    }
+}
+document.onload=verificador();
