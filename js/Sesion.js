fa30b422b954cb5717e84d8110775801e575bf22
Lempi4K <lemopi2008@gmail.com>
2022-04-11 22:26:04 -0600

    0.2: Inicio Decente

---
+++ js/Sesion.js
@@ -0,0 +1,12 @@
+function botonMandar(){
+    var usuario = document.getElementById("txtUsuario").value;
+    var pass = document.getElementById("txtPass").value;
+    if(usuario=="Lempi4K" && pass=="morenopinto23"){
+        alert("ta bien");
+    }
+    else{
+        alert("ta mal");
+    }
+}
+
+
